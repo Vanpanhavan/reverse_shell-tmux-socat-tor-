2e681f9ff783172533dfa1226db3e3c3c3b7d4f4
Vanpanhavan <160994983+Vanpanhavan@users.noreply.github.com>
2026-05-05 20:53:06 +0700

    Update Script

--- client.sh
+++ client.sh
@@ -25,7 +25,7 @@ fi
 
 PORT_PROXY_INITIAL_CONNECTION=$(ss -tulnp | awk '{print $5}' | grep 9999)
 if [[ -z "$PORT_PROXY_INITIAL_CONNECTION" ]]; then
-        socat TCP-LISTEN:9999,reuseaddr SOCKS5-CONNECT:localhost:9050:wznpsql....l7nsiv3xc4k6id.onion:4444 & # update this onion server
+        socat TCP-LISTEN:9999,reuseaddr SOCKS5-CONNECT:localhost:9050:wznpsql7....l7nsiv3xc4k6id.onion:4444 &
 fi
 
 
@@ -35,7 +35,7 @@ sleep 2
 
 PORT_PROXY_FOR_SEND_PROXY_PORT=$(ss -tulnp | awk '{print $5}' | grep 8888)
 if [[ -z "$PORT_PROXY_INITIAL_CONNECTION" ]]; then
-        socat TCP-LISTEN:8888,reuseaddr SOCKS5-CONNECT:localhost:9050:wznpsql....l7nsiv3xc4k6id.onion:5555 & # update this onion server
+        socat TCP-LISTEN:8888,reuseaddr SOCKS5-CONNECT:localhost:9050:wznpsql7....l7nsiv3xc4k6id.onion:5555 &
 fi
 
 for PORT_LISTEN in {30000..40000}; do
